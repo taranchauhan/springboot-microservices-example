9beca74399ebcf57235f338f569449faea90ffe2
Taran Chauhan <taran.chauhan@hotmail.co.uk>
2019-07-16 05:12:44 +0100

    Create Eureka discovery server and clients
    1. Create Eureka discovery server
    2. Enable Eureka clients for each microservice.

--- music-chart-service/src/main/java/io/java/spring/microservices/musicchartservice/MusicChartServiceApplication.java
+++ music-chart-service/src/main/java/io/java/spring/microservices/musicchartservice/MusicChartServiceApplication.java
@@ -2,10 +2,12 @@ package io.java.spring.microservices.musicchartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MusicChartServiceApplication {
 	
 	@Bean
